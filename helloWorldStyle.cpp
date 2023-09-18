9c5ee9e6cc4e20aa1dbd29d1d6c247434e4cf2e2
santiagohewett <santiago.hewett@stu.ocsb.ca>
2023-09-18 16:53:54 +0000

    Update Unit1-03-CPP

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Santiago Hewett All rights reserved.
+// .
+// Created by: Santiago Hewett
+// Date: Sep 18, 2023
+// This program displays "Hello, World with style."
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!\n";
+    std::cout << "C++ is a bit more complicated than Python.\n";
+}
